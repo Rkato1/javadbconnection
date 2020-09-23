1748c3d2f100a366dbbb3566e6add2fa746e8c4a
Rkato1 <36435326+Rkato1@users.noreply.github.com>
2020-09-23 16:07:32 +0900

    add

--- jdbc_workspace/jdbc1/src/member/controller/MemberController.java
+++ jdbc_workspace/jdbc1/src/member/controller/MemberController.java
@@ -23,6 +23,7 @@ public class MemberController {
 			System.out.println("3. 회원 등록");
 			System.out.println("4. 회원 정보 수정");
 			System.out.println("5. 회원 정보 삭제");
+			System.out.println("6. 회원 정보 검색(이름+포함되면 전부 다 조회)");
 			System.out.print("선택 > ");
 			int sel = sc.nextInt();
 			
@@ -34,17 +35,22 @@ public class MemberController {
 				selectMember();
 				break;
 			case 3:
+				insertMember();
 				break;
 			case 4:
 				break;
 			case 5:
+				deleteMember();
+				break;
+			case 6:
+				selectMembercontain();
 				break;
 			default:
 				break;
 			}
 		}
 	}
-	
+
 	public void selectAllMember() {
 		// TODO Auto-generated method stub
 		ArrayList<Member> list = dao.selectAllMember();
@@ -65,8 +71,64 @@ public class MemberController {
 		System.out.print("조회할 id 입력 : ");
 		String str = sc.next();
 		Member m = dao.selectMember(str);
-		System.out.println("조회 결과");
-		System.out.println("번호\t아이디\t비밀번호\t이름\t나이\t성별\t번호\t\t날짜");
-		System.out.println(m.getMemberNo()+"\t"+m.getMemberId()+"\t"+m.getMemberPw()+"\t"+m.getMemberName()+"\t"+m.getAge()+"\t"+m.getGender()+"\t"+m.getPhone()+"\t"+m.getEnroolDate());
+		if(m.getMemberId() == null) {
+			System.out.println("조회 불가");
+		}else {
+			System.out.println("조회 결과");
+			System.out.println("번호\t아이디\t비밀번호\t이름\t나이\t성별\t번호\t\t날짜");
+			System.out.println(m.getMemberNo()+"\t"+m.getMemberId()+"\t"+m.getMemberPw()+"\t"+m.getMemberName()+"\t"+m.getAge()+"\t"+m.getGender()+"\t"+m.getPhone()+"\t"+m.getEnroolDate());
+		}
+	}
+	
+	public void selectMembercontain() {
+		// TODO Auto-generated method stub
+		System.out.print("조회할 이름 입력(검색문자포함) : ");
+		String str = sc.next();
+		Member m = dao.selectMembercontain(str);
+		if(m.getMemberId() == null) {
+			System.out.println("조회 불가");
+		}else {
+			System.out.println("조회 결과");
+			System.out.println("번호\t아이디\t비밀번호\t이름\t나이\t성별\t번호\t\t날짜");
+			System.out.println(m.getMemberNo()+"\t"+m.getMemberId()+"\t"+m.getMemberPw()+"\t"+m.getMemberName()+"\t"+m.getAge()+"\t"+m.getGender()+"\t"+m.getPhone()+"\t"+m.getEnroolDate());
+		}
+	}
+
+	public void insertMember() {
+		// TODO Auto-generated method stub
+		System.out.println("=== 회원 등록 ===");
+		Member m = new Member();
+		System.out.print("id 입력 : ");
+		m.setMemberId(sc.next());
+		System.out.print("pw 입력 : ");
+		m.setMemberPw(sc.next());
+		System.out.print("이름 입력 : ");
+		m.setMemberName(sc.next());
+		System.out.print("번호 입력 : ");
+		m.setPhone(sc.next());
+		System.out.print("나이 입력 : ");
+		m.setAge(sc.nextInt());
+		System.out.print("성별 입력 : ");
+		m.setGender(sc.next().charAt(0));
+		int result = dao.insertMember(m);
+		if(result>0) {
+			System.out.println("등록성공");
+		}else {
+			System.out.println("등록실패");
+		}
+		System.out.println(result+"개 행에 적용됨");
+	}
+
+	public void deleteMember() {
+		// TODO Auto-generated method stub
+		System.out.print("삭제할 이름 입력 : ");
+		String str = sc.next();
+		int result = dao.deleteMember(str);
+		if(result>0) {
+			System.out.println("삭제성공");
+		}else {
+			System.out.println("삭제실패");
+		}
+		System.out.println(result+"개 행에 적용됨");
 	}
 }
--- jdbc_workspace/jdbc1/src/member/model/dao/MemberDao.java
+++ jdbc_workspace/jdbc1/src/member/model/dao/MemberDao.java
@@ -86,18 +86,17 @@ public class MemberDao {
 			stmt = conn.createStatement();
 			rset = stmt.executeQuery(query);
 			while(rset.next()) {
-				m = new Member();
 				//컬럼이름의 값을 데이터 형식에 맞춰서 받아옴(컬럼의 숫자여도 가능)				
 				int memberNo =	rset.getInt(1);
 				String memberId = rset.getString(2);
 				String memberPw = rset.getString(3);
 				String memberName = rset.getString(4);
-				int age = rset.getInt(5);
-				char gender = rset.getString(6).charAt(0);
-				String phone = rset.getString(7);
-				Date enrollDate = rset.getDate(8);
+				String phone = rset.getString(5);
+				int age = rset.getInt(6);
+				char gender = rset.getString(7).charAt(0);
+				Date enrollDate = rset.getDate(8);				
 				//5.값넣기
-				Member mem = new Member(memberNo,memberId,memberPw,memberName,age,gender,phone,enrollDate);
+				m = new Member(memberNo,memberId,memberPw,memberName,age,gender,phone,enrollDate);
 			}
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -117,6 +116,128 @@ public class MemberDao {
 			}
 		}
 		return m;
+	}
+
+	public Member selectMembercontain(String str) {
+		Member m = new Member();
+		String query = "select * from member where member_name like '%"+str+"%'";
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","jdbc","1234");
+			//타ip접근
+			//conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.10.23:1521:xe","jdbc","1234");
+			//System.out.println("conn : "+conn);
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(query);
+			while(rset.next()) {
+				int memberNo =	rset.getInt(1);
+				String memberId = rset.getString(2);
+				String memberPw = rset.getString(3);
+				String memberName = rset.getString(4);
+				String phone = rset.getString(5);
+				int age = rset.getInt(6);
+				char gender = rset.getString(7).charAt(0);
+				Date enrollDate = rset.getDate(8);				
+				//5.값넣기
+				m = new Member(memberNo,memberId,memberPw,memberName,age,gender,phone,enrollDate);
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {			
+			try {
+				//6.자원반환
+				rset.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return m;
+	}
+
+	public int insertMember(Member m) {
+		//insert/update/delete는 결과값이 정수형
+		int result = 0;
+		String query = "insert into member values(member_seq.nextval,'"+m.getMemberId()+"','"+m.getMemberPw()+"','"+m.getMemberName()+"','"+m.getPhone()+"',"+m.getAge()+",'"+m.getGender()+"', sysdate)";
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","jdbc","1234");
+			//System.out.println("conn : "+conn);
+			stmt = conn.createStatement();
+			//수행된 행의 개수
+			result = stmt.executeUpdate(query);
+			if(result > 0) {
+				conn.commit();
+			}else {
+				conn.rollback();
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {			
+			try {
+				//6.자원반환
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public int deleteMember(String str) {
+		int result = 0;
+		String query = "delete from member where member_name = '"+str+"'";
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","jdbc","1234");
+			//System.out.println("conn : "+conn);
+			stmt = conn.createStatement();
+			//수행된 행의 개수
+			result = stmt.executeUpdate(query);
+			if(result > 0) {
+				conn.commit();
+			}else {
+				conn.rollback();
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {			
+			try {
+				//6.자원반환
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 
 }
